90fbfc99b87397aad9f7acdd11d866d0d1adc297
Vinogradov Vyacheslav <Slava147148@gmail.com>
2020-10-28 20:26:20 +0300

    refactoring of the code

--- Program.cs
+++ Program.cs
@@ -6,26 +6,7 @@ namespace HomeWork
     {
         static void Main(string[] args)
         {
-            //Homework5.TestLogin();
-
-            //string test = "asdas abb, asdfs, aa, sad.";
-
-            //Message.WriteWordsWithLenghtLowN(test, 3);
-            //console.writeline("\n");
-            //console.writeline(message.getgreatestword(test));
-            //console.writeline("\n");
-            //console.writeline(message.getgreatestwordsstr(test));
-            //console.writeline("\n");
-            //Message.DeleteWordsEndCharFromText(ref test, 's');
-            //Console.WriteLine("\n");
-            //Console.WriteLine(test);
-            //Console.WriteLine("\n");
-            //Console.WriteLine(Message.CheckStrings("assd", "sads"));
-            //Console.WriteLine("\n");
-            //Console.WriteLine(Message.CheckStrings("assd", "dads"));
-
-            //Support.Pause();
-
+            
         }
     }
 }
