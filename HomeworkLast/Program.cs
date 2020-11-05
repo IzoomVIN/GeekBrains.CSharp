9a6f4a2e024ba364d576da7113c2c03409f04a7a
Vinogradov Vyacheslav <Slava147148@gmail.com>
2020-11-05 23:56:49 +0300

    tasts 1-2 of homework8 are complited

---
+++ HomeworkLast/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace HomeworkLast
+{
+    static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            //Task1.GetProperties();
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new HW8());
+        }
+    }
+}
---
+++ HomeworkLast/Task1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Reflection;
+
+namespace HomeworkLast
+{
+    class Task1
+    {
+        /// <summary>
+        /// <para>1</para>
+        /// <para>С помощью рефлексии выведите все свойства структуры DateTime</para>
+        /// </summary>
+        public static void GetProperties()
+        {
+            DateTime obj = new DateTime();
+            foreach( PropertyInfo inf in obj.GetType().GetProperties()) 
+            {
+                Console.WriteLine($"{inf.Name} : {inf.PropertyType}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
